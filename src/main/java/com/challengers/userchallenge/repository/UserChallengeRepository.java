349370e8fe5ff6e6efdae530fb243f92ac529498
JunSeong <kjs35323@naver.com>
2022-06-18 17:48:39 +0900

    feat : 챌린지 삭제,조회 기능 추가

--- src/main/java/com/challengers/userchallenge/repository/UserChallengeRepository.java
+++ src/main/java/com/challengers/userchallenge/repository/UserChallengeRepository.java
@@ -3,5 +3,9 @@ package com.challengers.userchallenge.repository;
 import com.challengers.userchallenge.domain.UserChallenge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserChallengeRepository extends JpaRepository<UserChallenge,Long> {
+    Optional<UserChallenge> findByUserIdAndChallengeId(Long userId, Long challengeId);
+    Long countByChallengeId(Long challengeId);
 }
