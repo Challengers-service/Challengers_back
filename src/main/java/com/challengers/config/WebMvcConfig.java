7a82e7a8ec330184b2e0420ccd1c06697ed0473a
ha-jinwoo <gkwlsdn95@naver.com>
2022-06-21 14:10:32 +0900

    feat : 무중단 배포 설정 파일, ProfileController 추가

--- src/main/java/com/challengers/config/WebMvcConfig.java
+++ src/main/java/com/challengers/config/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package com.challengers.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,16 +9,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
-    @Value("${app.cors.allowedOrigins}")
-    private String[] allowedOrigins;
-
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-        .allowedOrigins(allowedOrigins)
-        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .maxAge(MAX_AGE_SECS);
+    public void addCorsMappings(CorsRegistry registry){
+        registry
+                .addMapping("/**") // CORS 적용할 URL 패턴
+                .allowedOriginPatterns("*") //자원을 공유할 오리진 지정
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
