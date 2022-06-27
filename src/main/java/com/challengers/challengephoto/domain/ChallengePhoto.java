8149c29992f8a60cd17e4eb2c89cfbb1d2e92c83
JunSeong <kjs35323@naver.com>
2022-06-27 16:15:10 +0900

    feat : challenge_photo 테이블 생성 및 challenge_photo 업로드 기능 추가

---
+++ src/main/java/com/challengers/challengephoto/domain/ChallengePhoto.java
@@ -0,0 +1,43 @@
+package com.challengers.challengephoto.domain;
+
+import com.challengers.challenge.domain.Challenge;
+import com.challengers.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class ChallengePhoto {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String photoUrl;
+
+    @Builder
+    public ChallengePhoto(Long id, Challenge challenge, User user, String photoUrl) {
+        this.id = id;
+        this.challenge = challenge;
+        this.user = user;
+        this.photoUrl = photoUrl;
+    }
+
+    public static ChallengePhoto create(Challenge challenge, User user, String photoUrl) {
+        return ChallengePhoto.builder()
+                .challenge(challenge)
+                .user(user)
+                .photoUrl(photoUrl)
+                .build();
+    }
+}
