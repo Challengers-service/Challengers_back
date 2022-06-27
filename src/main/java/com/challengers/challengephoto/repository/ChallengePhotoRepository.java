a6a43b82d1b522b640c842c0244adf6b1f12d67b
JunSeong <kjs35323@naver.com>
2022-06-27 16:15:10 +0900

    feat : challenge_photo 테이블 생성 및 challenge_photo 업로드 기능 추가

---
+++ src/main/java/com/challengers/challengephoto/repository/ChallengePhotoRepository.java
@@ -0,0 +1,7 @@
+package com.challengers.challengephoto.repository;
+
+import com.challengers.challengephoto.domain.ChallengePhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengePhotoRepository extends JpaRepository<ChallengePhoto,Long> {
+}
